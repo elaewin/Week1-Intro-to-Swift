91c9a95e429b2b04dccd2510e09771e1aab53edf
Erica Winberry <elaewin@gmail.com>
2016-10-11 13:14:03 -0700

    working on first lab problem.

---
+++ labs/day2lab.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Write a function that takes in an array of planet names as strings, checks if array contains “Earth”. If it does not, add it. Otherwise, do nothing.
+
+var planetArray = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+func findEarth(arrayOfPlanets: [String]) {
+    
+    if !arrayOfPlanets.contains($0 == "Earth") {
+//        arrayOfPlanets += "Earth"
+    }
+}
+
+//Using sort and reduce, create string from the above array sorted alphabetically. Example Output: [“Venus”, “Earth”] would produce “Earth Venus”
+
+
+
+//Declare a dictionary of string keys and values. Declare a function that takes in a string and searches dictionary for it as a value.
+
+
+
+//Write a function that counts repeating words in an array of strings, and prints out the count of each
+
