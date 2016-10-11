30890014106912584d7d4c5933d1fe7b974893a0
Erica Winberry <elaewin@gmail.com>
2016-10-11 13:50:22 -0700

    Finished third lab problem.

--- labs/day2lab.playground/Contents.swift
+++ labs/day2lab.playground/Contents.swift
@@ -17,11 +17,43 @@ func findEarth(arrayOfPlanets: [String]) {
 
 //Using sort and reduce, create string from the above array sorted alphabetically. Example Output: [“Venus”, “Earth”] would produce “Earth Venus”
 
+func sortAndReduce(array: [String]) -> String {
+    
+    var sortedArray = array.sorted()
+    
+    
+    
+    return ""
+}
 
 
 //Declare a dictionary of string keys and values. Declare a function that takes in a string and searches dictionary for it as a value.
 
+let dictionaryOfPlanetGods = ["Mercury": "God of messengers and travel", "Venus": "Goddess of love and beauty", "Earth": "What is says on the label", "Mars": "God of war", "Jupiter": "God of thunder; Ruler of the gods", "Saturn": "God of agriculture", "Uranus": "Father of the gods", "Neptune": "God of the sea"]
+
+func whichGod(name: String) -> String {
+    
+    let name = name.capitalized
+    var result = ""
+    
+    if (dictionaryOfPlanetGods[name] != nil) {
+        result = dictionaryOfPlanetGods[name]!
+    } else {
+        result = "Not found."
+    }
+    
+    return result
+}
+
+whichGod(name: "Jupiter")
+whichGod(name: "mercury")
+whichGod(name: "Bill")
 
 
 //Write a function that counts repeating words in an array of strings, and prints out the count of each
+
+let arrayOfStrings = ["Climb a tree, wait for a fireman jump to fireman then scratch his face.", "Sleep on keyboard stand in front of the computer screen, but leave fur on owners clothes stand in front of the computer screen, yet instantly break out into full speed gallop across the house for no reason behind the couch.", "All of a sudden cat goes crazy ears back wide eyed.", "Fall over dead not really but gets sympathy find empty spot in cupboard and sleep all day."]
+
+
+
 
