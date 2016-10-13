601b697e29d84ea90aba566a039393ae422faadf
Erica Winberry <elaewin@gmail.com>
2016-10-13 13:36:44 -0700

    Starting lab

---
+++ labs/day4lab.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Declare Enum that conforms to Error. Your enum should have at least 5 cases and should use Associated Values.
+
+
+
+
+
+
+
+// Declare a Person class with 3 member properties. Declare a Student subclass that inherits from Person.
+// Both Your Student and Person class should have a variety of meaningful Type and Instance Methods. Be creative.
+// In either of these 2 classes, demonstrate a computed property and implement a property observer.
+
+
+
+
+
+
+
+// Create a Classroom class that is a true singleton and has an array of Students.
+// Add accessor methods to Classroom to add and remove from the array.
