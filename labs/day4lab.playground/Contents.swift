8e2242f5c0f73d5bb5fbabdc5ef339108451be5a
Erica Winberry <elaewin@gmail.com>
2016-10-13 15:43:46 -0700

    Person created, Student created, working on singleton.

--- labs/day4lab.playground/Contents.swift
+++ labs/day4lab.playground/Contents.swift
@@ -6,9 +6,9 @@ import UIKit
 
 enum LateForClass: Error {
 
-    case overslept(Int)
+    case alarmMalfunction(Int, Int)
     
-    case windstorm(String)
+    case weather(String)
     
     case carTrouble(String)
     
@@ -18,6 +18,20 @@ enum LateForClass: Error {
     
 }
 
+let excuse = LateForClass.busTrouble(5)
+
+switch excuse {
+case .alarmMalfunction(let alarmWentOff, let supposedToGoOff):
+    print("My alarm went off at \(alarmWentOff) instead of at \(supposedToGoOff)!")
+case .weather(let weatherType):
+    print("Sorry, the \(weatherType) was really bad at my house.")
+case .carTrouble(let automotiveIssue):
+    print("My car has \(automotiveIssue)--I couldn't get to class.")
+case .busTrouble(let busNumber):
+    print("The #\(busNumber) bus was running really late!")
+case .alienInvasion(let ridiculousAssertion):
+    print("Aliens are invading Earth! I had to \(ridiculousAssertion) before I could get to class.")
+}
 
 
 
@@ -25,11 +39,114 @@ enum LateForClass: Error {
 // Both Your Student and Person class should have a variety of meaningful Type and Instance Methods. Be creative.
 // In either of these 2 classes, demonstrate a computed property and implement a property observer.
 
+class Person {
+    
+    let firstName: String
+    let lastName: String
+    var age: Int
+    
+    init(firstName: String, lastName: String, age: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+    
+    func hadABirthday() {
+        self.age += 1
+    }
+    
+}
+
+var Me = Person(firstName: "Erica", lastName: "Winberry", age: 40)
 
 
+class Student : Person {
+    
+    var currentClass: String
+    
+    var assignmentGrades: [Double] {
+        didSet {
+            updateClassGrades()
+        }
+    }
+    
+    var currentGradePercent: Double
+    var letterGrade: Character
+    
+    init(currentClass: String,
+         assignmentGrades: [Double],
+         currentGradePercent: Double = 100.00,
+         letterGrade: Character = "A",
+         firstName: String,
+         lastName: String,
+         age: Int) {
+        
+        self.currentClass = currentClass
+        self.assignmentGrades = assignmentGrades
+        self.currentGradePercent = currentGradePercent
+        self.letterGrade = letterGrade
+        
+        super.init(firstName: firstName, lastName: lastName, age: age)
+    }
+    
+    var sumOfGradeScores: Double {
+        return assignmentGrades.reduce(0.0, {$0 + $1})
+    }
+    
+    func calcGradePercent() -> Double {
+        currentGradePercent = sumOfGradeScores / Double(assignmentGrades.count)
+        return currentGradePercent
+    }
+    
+    func addAssignmentGrade(newGrade: Double) {
+        assignmentGrades.append(newGrade)
+    }
+    
+    func getLetterGrade() {
+        switch calcGradePercent() {
+        case 90.0...100.0:
+            letterGrade = "A"
+        case 80.0...89.0:
+            letterGrade = "B"
+        case 70.0...79.0:
+            letterGrade = "C"
+        case 60.0...69.0:
+            letterGrade = "D"
+        default:
+            letterGrade = "F"
+        }
+    }
+    
+    func updateClassGrades() {
+        if !assignmentGrades.isEmpty {
+            calcGradePercent()
+            getLetterGrade()
+        } else {
+            print("No grades have been recorded.")
+        }
+    }
+    
+}
+
+
+var alsoMe = Student(currentClass: "iOS 401", assignmentGrades: [90.0, 89.0, 50.0], letterGrade: "B", firstName: "Erica", lastName: "Winberry", age: 40)
+
+alsoMe.currentGradePercent
+alsoMe.updateClassGrades()
+alsoMe.currentGradePercent
+
+alsoMe.letterGrade
+
+alsoMe.addAssignmentGrade(newGrade: 89.0)
+alsoMe.updateClassGrades()
+alsoMe.currentGradePercent
+
+alsoMe.letterGrade
 
 
 
 
 // Create a Classroom class that is a true singleton and has an array of Students.
 // Add accessor methods to Classroom to add and remove from the array.
+
+class Classroom
